6d6beac6a757d484ffa5830af70552dbb3e02015
Cindy Achieng <achiengcindy36@gmail.com>
2018-10-25 22:39:56 +0300

    Creating models
    Created models for the tasks

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('urgent', 'Urgent'),
+        ('normal', 'Normal'),
+    )
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='normal')
+    
+
+    class Meta:
+        verbose_name = 'task'
+        verbose_name_plural = 'tasks'
+        ordering = ('created',)
+
+    def __str__(self):
+        return self.title
+    
+     
